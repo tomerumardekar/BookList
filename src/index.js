ddb2498ac890410bb9b7986a8fbe9c571b7df60a
tomerumardekar <tomtom1361991@gmail.com>
2023-07-17 11:01:15 +0300

    booklist in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Book = (props) => {
+  const { img, title, author } = props.book;
+  return (
+    <article className="book">
+      <img src={img} alt="book children" />
+      <h1>{title}</h1>
+      <h4>{author} </h4>
+    </article>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    title: "What should Danny do?",
+    author: "Adir Levy , Ganit Levy",
+    img: "https://m.media-amazon.com/images/I/71zvriRVHrL._AC_UY218_.jpg",
+  },
+
+  {
+    title: "Kami Koala Makes A Decision",
+    author: "Bobbie Hinman",
+    img: "https://m.media-amazon.com/images/I/51OgwcPmGBL._AC_UY218_.jpg",
+  },
+
+  {
+    title: "Space Explorers: Outer Space Coloring and Activity Book for Kids",
+    author: "Y. Y. Riverman ",
+    img: "https://m.media-amazon.com/images/I/61i0VwvQA9L._AC_UY218_.jpg",
+  },
+
+  {
+    title: "Where is Home?",
+    author: "Natasha Blazic ",
+    img: "https://m.media-amazon.com/images/I/619ERQG7rIL._AC_UY218_.jpg",
+  },
+];
+
+function BookList() {
+  return (
+    <section className="bookList">
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}{" "}
+    </section>
+  );
+}
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
